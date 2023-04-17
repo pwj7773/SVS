f877bfeb1aeb9950b485f5415f3c3d7094be1d6c
pwj7773 <pwj7773@gmail.com>
2023-04-17 17:45:49 +0900

    feat: 댓글 ing

--- stayvision/src/main/java/com/svs/stayvision/service/reply/ReplyService.java
+++ stayvision/src/main/java/com/svs/stayvision/service/reply/ReplyService.java
@@ -2,13 +2,15 @@ package com.svs.stayvision.service.reply;
 
 import java.util.List;
 
+import com.svs.stayvision.util.PageNavigator;
 import com.svs.stayvision.vo.Reply;
 
 
 public interface ReplyService {
 	public int insertReply(Reply r);
-	public List<Reply> getAllReply(int boardNum);
+	public List<Reply> getAllReply(int boardNum,PageNavigator navi);
 	public Reply getOneReply(int replyNum);
 	public int updateReply(Reply r);
 	public int deleteReply(int replyNum);
+	public PageNavigator getPageNavigator(int pagePerGroup, int countPerPage, int page);
 }
