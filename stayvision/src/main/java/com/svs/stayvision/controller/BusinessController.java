6fde7f389b73bfd7921d8883bf2f22ef6d36410f
sinblack18 <jybin6356@naver.com>
2023-04-22 22:56:44 +0900

    test2

--- stayvision/src/main/java/com/svs/stayvision/controller/BusinessController.java
+++ stayvision/src/main/java/com/svs/stayvision/controller/BusinessController.java
@@ -46,11 +46,10 @@ public class BusinessController {
 		return "redirect:/loginafter";
 	}
 	
-
 	
 	// ROLE_ADMIN, ROLE_EMP의 전체 숙박업소 정보 조회
 	@GetMapping("/infoBusiness")
-	public String infoBusiness2(String category, String keyword, @AuthenticationPrincipal UserDetails user,Model model) {
+	public String infoBusiness(String category, String keyword, @AuthenticationPrincipal UserDetails user,Model model) {
 		
 		
 		List<Business> bList = bService.selectBusinessAll(category, keyword);
